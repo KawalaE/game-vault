f4cc68674400fcb2994c5f9bc1a74e68135c5762
KawalaE <edyta.wer.kawala@gmail.com>
2024-05-22 14:38:11 +0200

    Refactor: Extract a query objects

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -4,6 +4,7 @@ import { CanceledError } from "axios";
 import useData from "./useData";
 import { Genre } from "./useGenres";
 import { Platform } from "./usePlatforms";
+import { GameQuery } from "../App";
 export interface Platform {
   id: number;
   name: string;
@@ -17,19 +18,16 @@ export interface Game {
   metacritic: number;
 }
 
-const useGames = (
-  selectedGenre: Genre | null,
-  selectedPlatform: Platform | null
-) =>
+const useGames = (gameQuery: GameQuery) =>
   useData<Game>(
     "/games",
     {
       params: {
-        genres: selectedGenre?.id,
-        platforms: selectedPlatform?.id,
+        genres: gameQuery.genre?.id,
+        platforms: gameQuery.platform?.id,
       },
     },
-    [selectedGenre?.id, selectedPlatform?.id]
+    [gameQuery]
   );
 
 export default useGames;
