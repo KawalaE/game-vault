d4cac9636f2410dfa8ad55e22bd9ef62b01ef5ed
KawalaE <edyta.wer.kawala@gmail.com>
2024-05-22 13:30:46 +0200

    Filter games by platform

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,7 +3,7 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 import useData from "./useData";
 import { Genre } from "./useGenres";
-
+import { Platform } from "./usePlatforms";
 export interface Platform {
   id: number;
   name: string;
@@ -17,9 +17,19 @@ export interface Game {
   metacritic: number;
 }
 
-const useGames = (selectedGenre: Genre | null) =>
-  useData<Game>("/games", { params: { genres: selectedGenre?.id } }, [
-    selectedGenre?.id,
-  ]);
+const useGames = (
+  selectedGenre: Genre | null,
+  selectedPlatform: Platform | null
+) =>
+  useData<Game>(
+    "/games",
+    {
+      params: {
+        genres: selectedGenre?.id,
+        platforms: selectedPlatform?.id,
+      },
+    },
+    [selectedGenre?.id, selectedPlatform?.id]
+  );
 
 export default useGames;
