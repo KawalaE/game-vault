8b3ed1c17b06cb3840f90f8cde01850577f3e909
KawalaE <edyta.wer.kawala@gmail.com>
2024-06-04 13:06:34 +0200

    Build game attributes

--- src/components/GameDetailPage.tsx
+++ src/components/GameDetailPage.tsx
@@ -1,10 +1,13 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import ExpandableText from "./ExpandableText";
-
+import GameAttributes from "./GameAttributes";
+import DefinitionItem from "./DefinitionItem";
+import CriticScore from "./CriticScore";
 const GameDetailPage = () => {
   const { slug } = useParams();
+
   //(slug || '') or ! - which tells TS that this constant will never
   //be null
   const { data: game, isLoading, error } = useGame(slug!);
@@ -16,6 +19,7 @@ const GameDetailPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game}></GameAttributes>
     </>
   );
 };
